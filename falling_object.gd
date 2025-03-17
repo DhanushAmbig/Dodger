2f5be2e810f8b6f12c2d77fa64fca10716b2f9c9
DhanushAmbig <dhanu.ambig3@gmail.com>
2025-03-17 15:54:40 +0530

    Set boundary for player

--- falling_object.gd
+++ falling_object.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
-@export var fall_speed = 200  # Adjust speed
+@export var fall_speed = 300  # Adjust speed
 
 func _process(delta):
+	var screen_height = get_viewport_rect().size.y
 	position.y += fall_speed * delta  # Move downward
-	if position.y > 600:  # Delete if out of screen
+	if position.y > screen_height:  # Delete if out of screen
 		queue_free()
 
 func _on_body_entered(body):
